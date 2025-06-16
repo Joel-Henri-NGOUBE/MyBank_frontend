bec69ff90b5a57abd9011962bc201a49205e7c9d
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-06-16 18:33:19 +0200

    Page management introduction style over

--- frontend/src/Pages/Management/management.tsx
+++ frontend/src/Pages/Management/management.tsx
@@ -4,5 +4,25 @@ import "./management.css"
 export default function Management(){
     return <div className="management">
         <Header />
+
+        <div className="page">
+            {/* <div> */}
+                <p id="introduction">What are you using <span>MyBank</span> for today ?</p>
+                <div className="actions">
+                    <div className="tracking">
+                        <span>Tracking</span>
+                        <p>Just list and retrieve all the incomes and expenses</p>
+                    </div>
+                    <div className="investing">
+                        <span>Investing</span>
+                        <p>List and retrieve all the incomes and expenses and generate benefits from your balance </p>
+                    </div>
+                    <div className="saving">
+                        <span>Saving</span>
+                        <p>List and retrieve all the incomes and expenses and put some money aside from your balance</p>
+                    </div>
+                </div>
+            {/* </div> */}
+        </div>
     </div>
 }
