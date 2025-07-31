09f05166d2a28b0772093056b23837cceb6fbbc7
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-07-31 14:18:20 +0200

    Logout modification

--- frontend/src/Components/Header/header.tsx
+++ frontend/src/Components/Header/header.tsx
@@ -1,24 +1,18 @@
 import Company from "../Company/company";
 import WhiteBank from "../../assets/whitebank.svg"
 import "./header.css"
-import { Link, useLocation } from "react-router";
-import type { MouseEvent } from "react";
+import { Link, useLocation, useNavigate, type NavigateFunction } from "react-router";
 
 export default function Header(){
     const location = useLocation()
     const path = location.pathname
+
+    const navigate: NavigateFunction = useNavigate()
     // const {unused, pathname, unused2} = location
     // console.log(location.pathname)
-    function handleLogout(e: any){
-        const localToken = localStorage.getItem("token")
-        e.preventDefault()
-        fetch([`${import.meta.env.VITE_APP_BACKEND_API_URL}`, "/logout"].join(""), {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${localToken}`
-            }
-        })
+    function handleLogout(){
+        localStorage.removeItemItem("token")
+        navigate("/")
     }
 
     return <div className="header">
@@ -29,7 +23,7 @@ export default function Header(){
         <div className="links">
             <Link to="/operations" style={path === "/operations" ? {color: "var(--white)"} : {}}>operations</Link>
             <Link to="/management" style={path === "/management" ? {color: "var(--white)"} : {}}>management</Link>
-            <a href="" onClick={(e: any) => {handleLogout(e)}}>logout</a>
+            <a href="" onClick={() => {handleLogout()}}>logout</a>
             {/* <Link to="/settings" style={path === "/settings" ? {color: "var(--white)"} : {}}>settings</Link> */}
         </div>
     </div>
