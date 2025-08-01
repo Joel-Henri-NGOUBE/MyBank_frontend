54e56f28052044d8e303011fcac29fac6936cc7a
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-08-01 20:00:52 +0200

    Management adjustments

--- frontend/src/Components/ManagementComponents/Investing/investing.tsx
+++ frontend/src/Components/ManagementComponents/Investing/investing.tsx
@@ -6,12 +6,12 @@ import "./investing.css"
 export default function Investing({pages, setPages}: ManagementPages){
 
     const [investing, setInvesting] = useState({
-        amount: 0,
-        time: 0,
+        amount: "",
+        time: "",
         period: "days",
         date: formateDate(new Date(Date.now())),
-        interest: 0,
-        capital: 0,
+        interest: "",
+        capital: "",
         result: 0
     })
 
@@ -23,18 +23,24 @@ export default function Investing({pages, setPages}: ManagementPages){
     }, [investing.time, investing.amount, investing.date, investing.period, investing.capital, investing.interest])
 
     function getResult(investing: Investing){
+        let amount = parseFloat(investing.amount)
+        let interest = parseFloat(investing.interest)
+        let capital = parseFloat(investing.capital)
+        let time = parseFloat(investing.time)
         function getDays(firstDate: Date, secondDate: Date){
             const milliseconds =  secondDate.valueOf() - firstDate.valueOf()
+            console.log(milliseconds)
             const hours = parseInt(`${Math.floor(milliseconds / (1000 * 60 * 60))}`)
+            console.log(hours)
             return Math.floor(Math.abs(hours) / 24)
         }
         const days = getDays(new Date(investing.date), new Date(Date.now()))
-        if(investing.amount && investing.time && investing.capital && investing.interest > 0 && investing.time > 0 && investing.capital > 0 && investing.amount > 0){
-            const numberOfTimeInvested = investing.period === "days" ? Math.floor(days / investing.time) : Math.floor(Math.floor(days / 30) / investing.time)
+        if(amount && time && capital && interest && interest > 0 && time > 0 && capital > 0 && amount > 0){
+            const numberOfTimeInvested = investing.period === "days" ? Math.floor(days / time) : Math.floor(Math.floor(days / 30) / time)
             console.log(numberOfTimeInvested)
-            let investingResult = investing.capital
+            let investingResult = capital
             for(let i = 0; i < numberOfTimeInvested; i++){
-                investingResult = (investingResult + investing.amount) * (1 + (investing.interest / 100))
+                investingResult = (investingResult + amount) * (1 + (interest / 100))
             }
             setInvesting({...investing, result: investingResult})
         }
@@ -55,20 +61,20 @@ export default function Investing({pages, setPages}: ManagementPages){
         />
         <div className="body">
             <p>Combien souhaitez-vous mettre de côté pour épargne ?</p>
-            <input type="number" placeholder="1234,67" value={investing.amount} onChange={(e) => setInvesting((i) => ({...i, amount: parseInt(e.target.value)}))}/>
-            <p>Pendant quelle période souhaitez vous réaliser cette épargne  ?</p>
+            <input type="number" placeholder="1234,67" value={investing.amount} onChange={(e) => setInvesting((i) => ({...i, amount: e.target.value}))}/>
+            <p>A quelle fréquence souhaitez vous réaliser cette épargne  ?</p>
             <div className="period">
-                <input type="number" placeholder="1234" value={investing.time} onChange={(e) => setInvesting((i) => ({...i, time: parseInt(e.target.value)}))}/>
+                <input type="number" placeholder="1234" value={investing.time} onChange={(e) => setInvesting((i) => ({...i, time: e.target.value}))}/>
                 <select name="" id="" value={investing.period} onChange={(e) => setInvesting((i) => ({...i,  period: e.target.value}))}>
                     <option value="days">jours</option>
                     <option value="month">mois</option>
                 </select>
             </div>
-            <p>Quel capital prévoyez-vous d’avoir à cette période ? (Ne saisissez rien si capital actuel) ?</p>
-            <input type="number" placeholder="1234567" value={investing.capital} onChange={(e) => setInvesting((i) => ({...i,  capital: parseInt(e.target.value)}))}/>
+            <p>Quel est votre capital ?</p>
+            <input type="number" placeholder="1234567" value={investing.capital} onChange={(e) => setInvesting((i) => ({...i,  capital: e.target.value}))}/>
             <p>Quel pourcentage d’intérêt obtiendrez-vous sur votre capital (à chaque période)? </p>
-            <input type="number" placeholder="12,07" value={investing.interest} onChange={(e) => setInvesting((i) => ({...i,  interest: parseInt(e.target.value)}))}/>
-            <p>A partir de quelle date ?</p>
+            <input type="number" placeholder="12,07" value={investing.interest} onChange={(e) => setInvesting((i) => ({...i,  interest: e.target.value}))}/>
+            <p>Jusqu'à quelle date ?</p>
             <input type="date" value={formateDate((new Date(investing.date)))} onChange={(e) => setInvesting((i) => ({...i, date: formateDate(new Date(e.target.value))}))}/>
             <p>Le résultat de votre épargne sera de: </p>
             <span className="result">{investing.result.toFixed(2)} €</span>
--- frontend/src/Components/ManagementComponents/Saving/saving.tsx
+++ frontend/src/Components/ManagementComponents/Saving/saving.tsx
@@ -5,8 +5,8 @@ import "./saving.css"
 
 export default function Saving({pages, setPages}: ManagementPages){
     const [saving, setSaving] = useState({
-        amount: 0,
-        time: 0,
+        amount: "",
+        time: "",
         period: "days",
         date: formateDate(new Date(Date.now())),
         result: 0
@@ -19,15 +19,17 @@ export default function Saving({pages, setPages}: ManagementPages){
     }, [saving.time, saving.amount, saving.date, saving.period])
 
     function getResult(saving: Saving){
+        const amount = parseFloat(saving.amount)
+        const time = parseFloat(saving.time)
         function getDays(firstDate: Date, secondDate: Date){
             const milliseconds =  secondDate.valueOf() - firstDate.valueOf()
             const hours = parseInt(`${Math.floor(milliseconds / (1000 * 60 * 60))}`)
             return Math.floor(Math.abs(hours) / 24)
         }
         const days = getDays(new Date(saving.date), new Date(Date.now()))
-        if(saving.amount && saving.time && saving.amount > 0 && saving.time > 0){
-            const periodDifference = saving.period === "days" ? Math.floor(days / saving.time) : Math.floor(Math.floor(days / 30) / saving.time)
-            setSaving({...saving, result: periodDifference * saving.amount})
+        if(amount && time && amount > 0 && time > 0){
+            const numberOfTimeSaved = saving.period === "days" ? Math.floor(days / time) : Math.floor(Math.floor(days / 30) / time)
+            setSaving({...saving, result: numberOfTimeSaved * amount})
         }
         else{
             setSaving({...saving, result: 0})
@@ -46,10 +48,10 @@ export default function Saving({pages, setPages}: ManagementPages){
                 />
                 <div className="body">
                     <p>Combien souhaitez-vous mettre de côté pour épargne ?</p>
-                    <input type="number" placeholder="1234.67" value={saving.amount} onChange={(e) => setSaving({...saving, amount: parseInt(e.target.value)})}/>
-                    <p>Pour quelle fréquence souhaitez vous réaliser cette épargne  ?</p>
+                    <input type="number" placeholder="1234.67" value={saving.amount} onChange={(e) => setSaving({...saving, amount: e.target.value})}/>
+                    <p>A quelle fréquence souhaitez vous réaliser cette épargne  ?</p>
                     <div className="period">
-                        <input type="number" placeholder="1234" value={saving.time} onChange={(e) => setSaving({...saving, time: parseInt(e.target.value)})}/>
+                        <input type="number" placeholder="1234" value={saving.time} onChange={(e) => setSaving({...saving, time: e.target.value})}/>
                         <select name="" id="" value={saving.period} onChange={(e) => setSaving({...saving,  period: e.target.value})}>
                             <option value="days">jours</option>
                             <option value="month">mois</option>
@@ -58,7 +60,7 @@ export default function Saving({pages, setPages}: ManagementPages){
                     <p>Jusqu'à quelle date ?</p>
                     <input type="date" value={formateDate((new Date(saving.date)))} onChange={(e) => setSaving({...saving, date: formateDate(new Date(e.target.value))})}/>
                     <p>Le résultat de votre épargne sera de: </p>
-                    <span className="result">{saving.result} €</span>
+                    <span className="result">{saving.result.toFixed(2)} €</span>
                 </div>
             </div>
 }
